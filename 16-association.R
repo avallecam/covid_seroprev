726b1ed4967cfd20c0d56f652946bd8e77928b06
avallecam <avallecam@gmail.com>
2020-11-12 21:17:43 -0500

    add regression

---
+++ 16-association.R
@@ -0,0 +1,190 @@
+#'
+#' OBJETIVES:
+#' - create association estimates with survey design
+#' - table and figure output
+#' write_rds("data/____")
+#' write_xlsx("table/____")
+#' ggsave("figure/____")
+#' 
+
+library(tidyverse)
+library(skimr)
+library(survey)
+library(srvyr)
+# library(purrr)
+# library(furrr)
+# library(writexl)
+# library(tictoc)
+
+theme_set(theme_bw())
+
+
+# functions ---------------------------------------------------------------
+
+# source("10-prevalence_functions.R")
+
+# source("08-uncertainty_prev.R")
+
+outcome_to_numeric <- function(variable) {
+  as.numeric({{variable}})-1
+}
+
+library(serosurvey)
+
+set.seed(33)
+
+# inputs ------------------------------------------------------------------
+
+uu_clean_data <- read_rds("data/uu_clean_data.rds") %>% 
+  mutate(survey_all="survey_all",
+         weight_nul=1) %>% 
+  # transformar a factor (prevlaencia ajustada)
+  mutate_at(.vars = vars(igg,igm,ig_clasificacion,positividad_peru),
+            .funs = as.factor) %>% 
+  # transformar a numerico (prevalencia cruda)
+  mutate_at(.vars = vars(igg,igm,ig_clasificacion,positividad_peru),
+            .funs = list("num"=outcome_to_numeric)) #%>%
+# # extender respuestas por condicicion de riesgo
+# mutate_at(.vars = vars(starts_with("condicion_riesgo_")),
+#           .funs = list("ext"=~riesgo_extend_na(variable = .x,referencia = riesgo))) #%>% 
+# # de ord a fct
+# mutate(edad_decenios=as.character(edad_decenios),
+#        edad_decenios=as.factor(edad_decenios))
+
+uu_clean_data %>% 
+  count(ig_clasificacion,ig_clasificacion_num,positividad_peru) %>% 
+  avallecam::print_inf()
+
+# ___________ -------------------------------------------------------------
+
+# SELECT COVARIATES -------------------------------------------------------
+
+covariate_set01 <- uu_clean_data %>% 
+  select(survey_all,
+         sexo,
+         edad_etapas_de_vida_t,
+         edad_decenios,
+         # edad_quinquenal,
+         diris,
+         # pobreza_dico,
+         ind_hacin,
+         # ind_hacin_qrt,
+         ind_hacin_cut,
+         hacinamiento,
+         # nro_dormitorios_cat,
+         nm_prov,
+         sintomas_cualquier_momento_cat,
+         # sintomas_cualquier_momento_cat_fecha_14d_v1,
+         # sintomas_cualquier_momento_cat_fecha_rangos,
+         # riesgo,
+         # ends_with("_ext"),
+         # -contains("ninguna"),
+         # -contains("otro"),
+         # -contains("salud"),
+         # -contains("renal"),
+         # -contains("60a"),
+         contacto_covid,
+         contacto_covid_tipo,
+         prueba_previa,
+         prueba_previa_cat,
+         prueba_previa_res
+         # etnia_cat,
+         # trabajo_reciente,
+         # atencion,
+         # seguro_salud,
+  ) %>% 
+  colnames()
+
+
+# ____________ ------------------------------------------------------------
+
+# SEROPREVALENCIA ---------------------------------------------------------
+
+# tratamiento de stratos con un solo conglomerado
+options(survey.lonely.psu = "certainty")
+
+uu_clean_data %>% count(CONGLOMERADO,VIVIENDA)
+
+# diseño muestral de la encuesta ---------------------------------
+
+design <- uu_clean_data %>% 
+  
+  filter(!is.na(ig_clasificacion)) %>% #CRITICAL! ON OUTCOME
+  filter(!is.na(factorfinal)) %>% #NO DEBEN DE HABER CONGLOMERADOS SIN WEIGHT
+  
+  as_survey_design(c(CONGLOMERADO, VIVIENDA),
+                   #id = CONGLOMERADO, #clusters or psu (primary sampling unit) 
+                   #add vivienda como *SSU*
+                   strata = ESTRATO, #clusters need to be nested in the strata
+                   weights= PONDERACION # factores de expancion
+  )
+
+
+# ______________ ----------------------------------------------------------
+
+
+# models ------------------------------------------------------------------
+
+library(broom)
+library(epitidy)
+
+# null model --------------------------------------------------------------
+
+uu_clean_data %>% count(hacinamiento)
+uu_clean_data %>% pull(ig_clasificacion_num) %>% mean()
+
+glm_null <- svyglm(ig_clasificacion_num ~ 1, 
+                   design = design,
+                   family = poisson(link = "log")#,
+                   # na.action = na.exclude
+                   )
+
+glm_null %>% epi_tidymodel_pr()
+
+# # _ simple regression -----------------------------------------------------
+# 
+# # ?survey::svyglm
+# 
+# my_model <- svyglm(ig_clasificacion_num ~ hacinamiento,
+#                    design,
+#                    family = poisson(link = "log"))
+# # summary(my_model)
+# # tidy(my_model)
+# epitidy::epi_tidymodel_pr(my_model)
+# my_model <- survey::svyglm(ig_clasificacion_num ~ hacinamiento, design)
+# epitidy::epi_tidymodel_pr(my_model)
+# my_model <- survey::svyglm(ig_clasificacion_num ~ ind_hacin_cut, design)
+# epitidy::epi_tidymodel_pr(my_model)
+
+# more than one simple model ------------------------------------------------------------
+
+# crear matriz
+# set 01 of denominator-numerator
+simple_models <- expand_grid(
+  design=list(design),
+  # denominator=covariate_set01[c(6)],
+  denominator=covariate_set01[-c(1,5)],
+  numerator=c("ig_clasificacion_num")
+  ) %>%
+  # crear simbolos
+  mutate(
+    denominator=map(denominator,dplyr::sym),
+    numerator=map(numerator,dplyr::sym)
+  ) %>% 
+  #purrr::map
+  #create symbol, update null model, tidy up the results
+  mutate(variable=map(denominator,dplyr::sym),
+         simple_rawm=map(.x = variable, .f = epi_tidymodel_up, reference_model=glm_null),
+         simple_tidy=map(.x = simple_rawm, .f = epi_tidymodel_pr)
+  ) %>%
+  #unnest coefficients
+  unnest(cols = c(simple_tidy)) %>%
+  #filter out intercepts
+  filter(term!="(Intercept)")
+
+simple_models %>% 
+  select(-(1:5)) %>% 
+  avallecam::print_inf()
+
+
+
